1c3612e51a86a44778935b576c37779b8ad81d58
Matias <gomezmatias126@gmail.com>
2023-08-01 18:32:26 -0300

    Animaciones para el hero agregadas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()]
